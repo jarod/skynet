061ef3de94ffe86c7deb91f4fc058b26c25a0446
Jarod Liu <liuyuanzhi@gmail.com>
2014-07-10 15:16:23 +0800

    matrix tcp client code cleanup

--- skynet-agent/matrix_tcp.go
+++ skynet-agent/matrix_tcp.go
@@ -1,52 +1,107 @@
 package main
 
 import (
+	"bytes"
 	"code.google.com/p/goprotobuf/proto"
 	"encoding/json"
 	"github.com/jarod/skynet/skynet"
 	skc "github.com/jarod/skynet/skynet/client"
-	skmc "github.com/jarod/skynet/skynet/matrix/client"
 	skn "github.com/jarod/skynet/skynet/net"
+	"io"
 	"log"
+	"net"
 	"os/exec"
 	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	maxRetryDelay = time.Second * 32
 )
 
 type MatrixClient struct {
-	mc *skmc.MatrixClient
+	conn *net.TCPConn
+	cond *sync.Cond
 }
 
-func DialMatrix(addr string) (*MatrixClient, error) {
-	mc, err := skmc.Dial(addr)
+func DialMatrix(raddr string) (mc *MatrixClient, err error) {
+	addr, err := net.ResolveTCPAddr("tcp", raddr)
 	if err != nil {
-		return nil, err
+		return
 	}
-	c := &MatrixClient{mc: mc}
-	go c.readMatrix()
-	return c, nil
+	mc, err = newMatrixClient()
+	if err != nil {
+		return
+	}
+	mc.connect(addr)
+	return
 }
 
-func (m *MatrixClient) readMatrix() {
-	for {
-		p, err := m.mc.Read()
+func newMatrixClient() (mc *MatrixClient, err error) {
+	mc = new(MatrixClient)
+	mc.cond = sync.NewCond(&sync.Mutex{})
+	return
+}
+
+func (mc *MatrixClient) connect(raddr *net.TCPAddr) {
+	connDelay := time.Second
+	for i := uint32(0); ; i++ {
+		conn, err := net.DialTCP("tcp", nil, raddr)
 		if err != nil {
-			log.Printf("readMatrix err=%v", err)
+			if connDelay < maxRetryDelay {
+				connDelay *= 2
+			}
+			if i%4 == 0 {
+				log.Printf("Failed to connect Matrix server %v, reconnect in %v", raddr, connDelay)
+			}
+			time.Sleep(connDelay)
+		} else {
+			log.Printf("Connected to Matrix[%s]", conn.RemoteAddr())
+			go mc.onConnected(conn)
+			break
+		}
+	}
+}
+
+func (m *MatrixClient) onConnected(conn *net.TCPConn) {
+	m.cond.L.Lock()
+	m.conn = conn
+	m.cond.Broadcast()
+	m.cond.L.Unlock()
+	for {
+		p, err := skn.ParsePacket(conn)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("onConnected - %v\n", err)
+			}
 			break
 		}
 		m.dispatchMessage(p)
 	}
+	conn.Close()
+	m.cond.L.Lock()
+	m.conn = nil
+	m.cond.L.Unlock()
+	go m.connect(conn.RemoteAddr().(*net.TCPAddr))
 }
 
-func (m *MatrixClient) Write(p *skn.Packet) {
-	m.mc.Write(p)
+func (mc *MatrixClient) Write(p *skn.Packet) {
+	data := p.Encode()
+	mc.cond.L.Lock()
+	if mc.conn == nil {
+		mc.cond.Wait()
+	}
+	io.Copy(mc.conn, bytes.NewReader(data))
+	mc.cond.L.Unlock()
 }
 
 func (m *MatrixClient) dispatchMessage(p *skn.Packet) {
 	switch p.Head {
 	case 0x0000:
-		m.onMatrixAppInfoUpdate(p)
+		m.onAppInfoUpdate(p)
 	case 0x0001:
-		m.onMatrixAppDisconnect(p)
+		m.onAppDisconnect(p)
 	case 0x0002:
 		m.execAgentCmd(p)
 	default:
@@ -54,21 +109,21 @@ func (m *MatrixClient) dispatchMessage(p *skn.Packet) {
 	}
 }
 
-func (m *MatrixClient) onMatrixAppInfoUpdate(p *skn.Packet) {
+func (m *MatrixClient) onAppInfoUpdate(p *skn.Packet) {
 	info := new(skc.AppInfo)
 	err := json.Unmarshal(p.Body, info)
 	if err != nil {
-		log.Println("onMatrixAppInfoUpdate - ", err)
+		log.Println("onAppInfoUpdate - ", err)
 		return
 	}
 	appInfos[info.Id] = info
 }
 
-func (m *MatrixClient) onMatrixAppDisconnect(p *skn.Packet) {
+func (m *MatrixClient) onAppDisconnect(p *skn.Packet) {
 	id := new(skynet.Pstring)
 	err := proto.Unmarshal(p.Body, id)
 	if err != nil {
-		log.Println("onMatrixAppDisconnect - ", err)
+		log.Println("onAppDisconnect - ", err)
 		return
 	}
 	delete(appInfos, id.GetValue())
@@ -79,7 +134,7 @@ func (m *MatrixClient) execAgentCmd(p *skn.Packet) {
 		c := new(skynet.Pstring)
 		proto.Unmarshal(p.Body, c)
 
-		log.Println("exec cmd=", c.GetValue())
+		log.Println("execAgentCmd", c.GetValue())
 		rawCmd := strings.Split(c.GetValue(), " ")
 		cmd := exec.Command(rawCmd[0], rawCmd[1:]...)
 		data, err := cmd.CombinedOutput()
